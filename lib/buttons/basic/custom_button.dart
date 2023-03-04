5e70c78579fa70c88fff25c8310def547d93c34a
AlexanderHorielko <s.gorelko1@gmail.com>
2023-03-04 21:41:43 +0200

    refactor(buttons): update buttons style

--- lib/buttons/basic/custom_button.dart
+++ lib/buttons/basic/custom_button.dart
@@ -179,60 +179,70 @@ class _CustomButtonState extends State<CustomButton> {
           if (widget.state == ButtonState.disabled) return;
         },
         borderRadius: 48,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(48),
-            border: widget.type == ButtonType.outline
-                ? Border.all(color: ButtonColors.primaryBase)
+        child: IntrinsicWidth(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(48),
+              border: widget.type == ButtonType.outline
+                  ? Border.all(color: ButtonColors.primaryBase)
+                  : null,
+              color: _getButtonColor(),
+            ),
+            width: widget.size == ButtonSize.block
+                ? min(327, MediaQuery.maybeOf(context)!.size.width)
                 : null,
-            color: _getButtonColor(),
-          ),
-          width: widget.size == ButtonSize.block
-              ? min(327, MediaQuery.maybeOf(context)!.size.width)
-              : null,
-          padding: EdgeInsets.symmetric(
-            vertical: widget.size == ButtonSize.small
-                ? 8
-                : widget.icon != null
-                    ? 12
-                    : 16,
-            horizontal: 16,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              if (widget.icon != null &&
-                  widget.iconPosition == ButtonIconPosition.left)
-                SvgPicture.asset(
-                  widget.icon!,
-                  width: _iconSize,
-                  height: _iconSize,
-                ),
-              Row(
-                children: [
-                  if (widget.icon != null &&
-                      widget.iconPosition == ButtonIconPosition.side)
-                    SvgPicture.asset(
+            padding: EdgeInsets.symmetric(
+              vertical: widget.size == ButtonSize.small
+                  ? 8
+                  : widget.icon != null
+                      ? 12
+                      : 16,
+              horizontal: 16,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (widget.icon != null)
+                  Opacity(
+                    opacity:
+                        widget.iconPosition == ButtonIconPosition.left ? 1 : 0,
+                    child: SvgPicture.asset(
                       widget.icon!,
                       width: _iconSize,
                       height: _iconSize,
                     ),
-                  Text(
-                    widget.text,
-                    style: MyFonts.m.copyWith(
-                      color: _getButtonTextColor(),
-                    ),
-                  )
-                ],
-              ),
-              if (widget.icon != null &&
-                  widget.iconPosition == ButtonIconPosition.right)
-                SvgPicture.asset(
-                  widget.icon!,
-                  width: _iconSize,
-                  height: _iconSize,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (widget.icon != null &&
+                        widget.iconPosition == ButtonIconPosition.side)
+                      SvgPicture.asset(
+                        widget.icon!,
+                        width: _iconSize,
+                        height: _iconSize,
+                      ),
+                    const SizedBox(width: 8),
+                    Text(
+                      widget.text,
+                      style: MyFonts.m.copyWith(
+                        color: _getButtonTextColor(),
+                      ),
+                    )
+                  ],
                 ),
-            ],
+                if (widget.icon != null)
+                  Opacity(
+                    opacity:
+                        widget.iconPosition == ButtonIconPosition.left ? 1 : 0,
+                    child: SvgPicture.asset(
+                      widget.icon!,
+                      width: _iconSize,
+                      height: _iconSize,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
